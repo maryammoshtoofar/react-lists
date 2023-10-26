da0fbb1288d879d5f847f308c79e245eb65d8c7c
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2023-10-26 11:39:07 +0330

    add loader

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,5 +1,14 @@
+import Spinner from "react-bootstrap/Spinner";
+
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <>
+      <h1 className="m-auto mb-3">React Lists App</h1>
+      <Spinner animation="border" role="status" className="m-auto">
+        <span className="visually-hidden">Loading</span>
+      </Spinner>
+    </>
+  );
 };
 
 export default Loader;
