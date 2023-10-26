366220b6c32cd916c11e2f77ce1d369a6da551c3
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2023-10-26 13:07:33 +0330

    add uuid

--- src/App.js
+++ src/App.js
@@ -2,14 +2,47 @@ import Wrapper from "./layout/wrapper";
 import Header from "./layout/header";
 import TabsContaier from "./components/tabs";
 import Items from "./components/items";
+import Loader from "./components/loader";
+import { useState } from "react";
+import { lists } from "./data";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [allData, setAllData] = useState(lists);
+  const [active, setActive] = useState("1");
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 2000);
+
+  const handleActiveList = (k) => setActive(k);
+
+  const selectedList = allData.find((list) => list.id === active).data;
+
+  const addItemsToData = (newItem, id) => {
+    const selectedList = allData.find((item) => item.id === id);
+    selectedList.data.push(newItem);
+    const index = allData.findIndex((item) => item.id === id);
+    const updatedList = [...allData];
+    updatedList.splice(index, 1, selectedList);
+    setAllData(updatedList);
+  };
   return (
     <Wrapper>
-      <Header>
-        <TabsContaier />
-      </Header>
-      <Items />
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header>
+            <TabsContaier
+              allData={allData}
+              active={active}
+              handleActiveList={handleActiveList}
+            />
+          </Header>
+          <Items active={active} selectedList={selectedList} addToData={addItemsToData} />
+        </>
+      )}
     </Wrapper>
   );
 };
