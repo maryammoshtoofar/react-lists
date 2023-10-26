bd528165975f77f741a5227fe28dd53e2fc03012
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2023-10-26 10:58:19 +0330

    create mock tabs

--- src/components/addList/index.js
+++ src/components/addList/index.js
@@ -1,5 +1,16 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 const AddList = () => {
-  return <div>AddList</div>;
+  return (
+    <Form className="d-flex">
+      <Form.Group className="w-100 me-2" controlId="formBasicEmail" >
+        <Form.Control type="text" />
+      </Form.Group>
+      <Button variant="success" type="submit">
+        ADD
+      </Button>
+    </Form>
+  );
 };
 
 export default AddList;
