aa0513a9a0a774b2dac59395f7e781f7c8a22194
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2023-10-26 11:32:10 +0330

    add initial data

---
+++ data.js
@@ -0,0 +1,38 @@
+export const lists = [
+  {
+    id: "1",
+    value: "To-dos",
+    data: [
+      {
+        id: 1,
+        title: "Study for tomorrow's exam",
+        isCompleted: true,
+      },
+      {
+        id: 2,
+        title: "Do Laundry",
+        isCompleted: false,
+      },
+      {
+        id: 3,
+        title: "Send Application Emails",
+        isCompleted: true,
+      },
+      {
+        id: 4,
+        title: "Call Mark",
+        isCompleted: false,
+      },
+    ],
+  },
+  {
+    id: "2",
+    value: "Groceries",
+    data: [
+      { id: 1, title: "Milk", isCompleted: true },
+      { id: 2, title: "Sugar", isCompleted: false },
+      { id: 3, title: "Bread", isCompleted: true },
+      { id: 4, title: "Tomatoes", isCompleted: false },
+    ],
+  },
+];
