bc443b0421f51504dd8d413699c16489bcb01cff
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2023-10-26 10:59:41 +0330

    create basic tabs

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -1,5 +1,19 @@
-const Tabs = () => {
-  return <div>Tabs</div>;
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+
+const TabsContaier = () => {
+  return (
+    <Tabs
+      defaultActiveKey="profile"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="home" title="Home">
+      </Tab>
+      <Tab eventKey="profile" title="Profile">
+      </Tab>
+    </Tabs>
+  );
 };
 
-export default Tabs;
+export default TabsContaier;
